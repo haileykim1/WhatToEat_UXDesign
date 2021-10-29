e4992e9f8d2e59c6b6e31bb8be5980f84642a8eb
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2021-10-30 03:47:04 +0900

    Build UI : MainActivity.kt

--- app/src/main/java/com/sy/whattoeat/MainActivity.kt
+++ app/src/main/java/com/sy/whattoeat/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sy.whattoeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.sy.whattoeat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        supportActionBar!!.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar!!.setCustomView(R.layout.action_bar_layout)
     }
 }
